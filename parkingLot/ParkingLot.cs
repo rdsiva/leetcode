da385953ae257cef745b7139c1a3fdcc65a07055
siva Dhanapal <rdsiva@gmail.com>
2025-01-11 19:55:39 -0600

    parkinglot

---
+++ parkingLot/ParkingLot.cs
@@ -0,0 +1,27 @@
+﻿public class ParkingSystem
+{
+
+    public int[] spacesLeft;
+    public int medium = 0;
+    public int small = 0;
+    public ParkingSystem(int big, int medium, int small)
+    {
+        spacesLeft = new int[3] { big, medium, small };
+    }
+
+    public bool AddCar(int carType)
+    {
+        if (spacesLeft[carType - 1] > 0)
+        {
+            spacesLeft[carType - 1]--;
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj.AddCar(carType);
+ */
---
+++ parkingLot/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+var parking = new ParkingSystem(1,1,0);
+
+Console.WriteLine($"[{parking.AddCar(1)},{parking.AddCar(2)}, {parking.AddCar(3)},{parking.AddCar(1)}]");
+Console.ReadLine();
